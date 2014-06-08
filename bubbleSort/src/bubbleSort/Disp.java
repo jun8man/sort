065597067c30da2402fcc8ecd41967fce14b5f0f
jun8man <junya0220yamashita@gmail.com>
2014-06-09 00:57:50 +0900

    Update to get input data from external file.
    To get external input data, I have used "List" to avoid fixed size 
    data box with array.

--- bubbleSort/src/bubbleSort/Disp.java
+++ bubbleSort/src/bubbleSort/Disp.java
@@ -1,11 +1,13 @@
 package bubbleSort;
 
+import java.util.List;
+
 public class Disp {
 	
-	public static void out(int[] array){
+	public static void out(List<Integer> array){
 				
-		for(int i = 0; i < array.length; i++){
-			System.out.print("[" + i + "] : " + array[i] + " ");
+		for(int i = 0; i < array.size(); i++){
+			System.out.print("[" + i + "] : " + array.get(i) + " ");
 		}
 		System.out.println();
 	}
