cb83877eef2c7ad3326e820c185ea358d274e7ca
jun8man <junya0220yamashita@gmail.com>
2014-06-15 01:07:39 +0900

    Added new library to get input file and display the result of sort.

--- bubbleSort/src/bubbleSort/BubbleSort.java
+++ bubbleSort/src/bubbleSort/BubbleSort.java
@@ -1,12 +1,13 @@
 package bubbleSort;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+
+import getTargetList.getTargetList;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import display.Disp;
 
 /**
  * To test the Bubble Sort.
@@ -19,35 +20,11 @@ public class BubbleSort {
 	public static void main(String[] args) throws IOException{
 		
 		// Create array.
-		//int[] targets = {2, 1, 5, 4, 3};
 		List<Integer> targets = new ArrayList<Integer>();
-		
-		// Prepare to input data.
-		String inputFileName = "C:/cygwin/home/Junya/GitRepo/sort/input.data";
-		File inFile = new File(inputFileName);
+		targets = getTargetList.get();
 
-		// Read and store in array.
-		try {
-			FileReader fr = new FileReader(inFile);
-			BufferedReader br = new BufferedReader(fr);
-			
-			String str = null;
-			while( ( str = br.readLine() ) != null ){
-				// Parse string to integer and add to list.
-				targets.add(Integer.parseInt(str));
-			}
-			// close BufferReader Object.
-			br.close();
-		} catch (FileNotFoundException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
-		
-		// Disp the target.
-		System.out.print("Initial Data: ");
-		Disp.out(targets);
-		
-		int flag = 0;
+		int flag = 0; // Index for Head.
+		int count = 0; // To count swapping. 
 		
 		// Sort.
 		while(flag < targets.size()){
@@ -59,6 +36,7 @@ public class BubbleSort {
 					int tmp = targets.get(index);
 					targets.set(index, targets.get(index + 1));
 					targets.set((index + 1), tmp);
+					count++;
 				}
 				index++;
 			}
@@ -68,6 +46,7 @@ public class BubbleSort {
 		// Display the result of sort.
 		System.out.print("Final Result : ");
 		Disp.out(targets);
+		System.out.print("Total Swapping : " + count);
 	}
 
 }
--- display/src/display/Disp.java
+++ display/src/display/Disp.java
@@ -1,4 +1,4 @@
-package bubbleSort;
+package display;
 
 import java.util.List;
 
